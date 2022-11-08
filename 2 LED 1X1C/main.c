5439c7248b111d2aca123a6830e5e8148672ec72
Dennis <740125aa@gmail.com>
2022-11-08 21:54:14 +0800

    modify

--- 2 LED 1X1C/main.c
+++ 2 LED 1X1C/main.c
@@ -1,24 +1,24 @@
-/*以下程序为8路LED流水灯闪动程序，晶振频率为10MHz*/
+/*以下程序8路LED流水燈閃燈程序，晶振频率为10MHz*/
 #include <p18cxxx.h>
 #include "k18.h"
 #include "Delay.h"
 void main(void)
 {
-unsigned char a = 0x01;
-unsigned char  b;
+	unsigned char a = 0x01;
+	unsigned char b;
 
-k18_init();/*HL-K18主板初始化*/
+	k18_init();/*HL-K18主板初始化*/
 
-TRISD=0X00;/*设置D口为输出*/
+	TRISD=0X00;/*設置PORTD為输出*/
 
-COL1=1;/*选通点阵管的第一列的LED，点阵管的第一列的LED作为显示LED*/
+	COL1=1;/*選點陣管的第一列的LED，點陣管的第一列的LED作為顯示LED*/
 
-while(1)
+	while(1)
 	{
-	PORTD=0B00000001;//16进制写法0xfe  2进制写法0B00000001
- 	/*点阵管亮或灭*/
-	Delay10Ms(5);/*延时50mS*/
-	PORTD=0B00000000; //0x00; 0B00000000
-	Delay10Ms(5);/*延时50mS*/
- 	} 
+		PORTD=0B00000001;//16進制0xfe  2進制寫法0B00000001
+		/*點陣管亮或滅*/
+		Delay10Ms(5);/*延时50mS*/
+		PORTD=0B00000000; //0x00; 0B00000000
+		Delay10Ms(5);/*延时50mS*/
+	} 
 }
